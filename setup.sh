8164dadcade702e7428ca8046e385a865866ac36
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2022-11-04 14:19:34 +0000

    Update current feature

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ set -euxo pipefail
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 NS="ingress-nginx"
+NSAPP="ingress-app"
 
 NODE1_IP=$(kubectl get nodes --selector="! node-role.kubernetes.io/master" \
     -o=jsonpath='{.items[0].status.addresses[0].address}')
@@ -14,8 +15,10 @@ NODE1_IP=$(kubectl get nodes --selector="! node-role.kubernetes.io/master" \
 # Run on kubeadm cluster
 # see "kubernetes in action" p391
 kubectl delete ns -l "ingress=nginx"
-kubectl create namespace "$NS"
+kubectl create namespace "$NS" "$NSWORK"
+kubectl create namespace "$NSAPP"
 kubectl label ns "$NS" "ingress=nginx"
+kubectl label ns "$NSAPP" "ingress=nginx"
 
 # Install nginx-controller
 helm upgrade --install ingress-nginx ingress-nginx \
@@ -23,20 +26,20 @@ helm upgrade --install ingress-nginx ingress-nginx \
   --namespace "$NS" --create-namespace
 
 # Deploy application
-kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
-kubectl expose deployment web --type=NodePort --port=8080
+kubectl create deployment web -n "$NSAPP" --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment web -n "$NSAPP" --type=NodePort --port=8080
 kubectl  wait --for=condition=available deployment web
 
 # Wait for nginx-controller to be up and running
 kubectl wait --for=condition=available deployment -n "$NS" -l app.kubernetes.io/instance=ingress-nginx
 
 # Create ingress route
-kubectl apply -f https://k8s.io/examples/service/networking/example-ingress.yaml
-kubectl get ingress
+kubectl apply -n "$NSAPP" -f https://k8s.io/examples/service/networking/example-ingress.yaml
+kubectl get -n "$NSAPP" ingress
 
 echo "WARNING: Add the following line to /etc/hosts"
 echo "$NODE1_IP hello-world.info"
 
-NODE_PORT=$(kubectl get svc web -n "$NS"  -o jsonpath="{.spec.ports[0].nodePort}")
+NODE_PORT=$(kubectl get svc web -n "$NSAPP"  -o jsonpath="{.spec.ports[0].nodePort}")
 echo "INFO: access the application via ingress"
 echo "curl hello-world.info:$NODE_PORT"
