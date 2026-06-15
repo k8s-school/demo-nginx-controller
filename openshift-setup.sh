3bbd6a8a1eedbb9f6766a42436f8fbe00b078b6e
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-06-15 17:39:18 +0200

    Update current feature

--- openshift-setup.sh
+++ openshift-setup.sh
@@ -8,10 +8,7 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 NSAPP="ingress-app"
 
-NODE1_IP=$(kubectl get nodes -o=jsonpath='{.items[0].status.addresses[0].address}')
-
-# Run on kubeadm cluster
-# see "kubernetes in action" p391
+# Run on openshift cluster
 kubectl delete project -l "kubernetes.io/metadata.name=$NSAPP"
 oc new-project "$NSAPP"
 
@@ -21,13 +18,7 @@ kubectl expose deployment web -n "$NSAPP" --port=8080
 kubectl  wait -n "$NSAPP" --for=condition=available deployment web
 
 # Create ingress route
-kubectl apply -n "$NSAPP" -f $DIR/example-ingress.yaml
+kubectl apply -n "$NSAPP" -f $DIR/example-ingress-openshift.yaml
 kubectl get -n "$NSAPP" ingress
 
-TMP_STR="$NODE1_IP hello-world.info"
-echo "INFO: Add '$TMP_STR' to /etc/hosts"
-sudo sh -c "echo '$TMP_STR' >> /etc/hosts"
-
-NODE_PORT=$(kubectl get svc ingress-nginx-controller -n "$NS"  -o jsonpath="{.spec.ports[0].nodePort}")
-echo "INFO: access the application via ingress"
-curl hello-world.info:$NODE_PORT
+curl https://hello-world.apps-crc.testing
